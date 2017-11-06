d740f806aa2752abcaeaa054642feb3bab7a7605
Igor Breno <fuzzyqu@protonmail.com>
2017-11-06 16:40:52 -0300

    cache lines! yay

--- main.go
+++ main.go
@@ -9,22 +9,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type cacheLine struct {
+	integral models.Integral
+	n        int64
+}
+
 func main() {
 	router := gin.Default()
 
+	cache := make(map[cacheLine]float64, 128)
+
 	router.POST("/trapezio/:n", func(c *gin.Context) {
 		n := c.Param("n")
-		nInteger, err := strconv.ParseInt(n, 0, 64)
+		nint, err := strconv.ParseInt(n, 0, 64)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		}
 		var integral models.Integral
 		if err := c.ShouldBindJSON(&integral); err == nil {
-			result, err := metodos.RegraDosTrapezios(integral, nInteger)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			} else {
-				c.JSON(http.StatusOK, gin.H{"result": result})
+			}
+			if r, ok := cache[cacheLine{integral, nint}]; ok { // está na cache
+				c.JSON(http.StatusOK, gin.H{"result": r})
+			} else { // não foi computado ainda
+				result, err := metodos.RegraDosTrapezios(integral, nint)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				} else {
+					cache[cacheLine{integral, nint}] = result
+					c.JSON(http.StatusOK, gin.H{"result": result})
+				}
 			}
 		} else {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
