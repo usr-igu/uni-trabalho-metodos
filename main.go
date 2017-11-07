fc4c4f2922fe2dca02562ff192baec510a0316f3
fuzzyqu <fuzzyqu@protonmail.com>
2017-11-07 15:03:08 -0300

    simpson13 implementado

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type cacheLine struct {
+	method   string
 	integral models.Integral
 	n        int64
 }
@@ -27,17 +28,38 @@ func main() {
 		}
 		var integral models.Integral
 		if err := c.ShouldBindJSON(&integral); err == nil {
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			}
-			if r, ok := cache[cacheLine{integral, nint}]; ok { // está na cache
+			if r, ok := cache[cacheLine{"trapezio", integral, nint}]; ok { // está na cache ?
 				c.JSON(http.StatusOK, gin.H{"result": r})
 			} else { // não foi computado ainda
 				result, err := metodos.RegraDosTrapezios(integral, nint)
 				if err != nil {
 					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				} else {
-					cache[cacheLine{integral, nint}] = result
+					cache[cacheLine{"trapezio", integral, nint}] = result
+					c.JSON(http.StatusOK, gin.H{"result": result})
+				}
+			}
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	})
+
+	router.POST("/simpson13/:n", func(c *gin.Context) {
+		n := c.Param("n")
+		nint, err := strconv.ParseInt(n, 0, 64)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		var integral models.Integral
+		if err := c.ShouldBindJSON(&integral); err == nil {
+			if r, ok := cache[cacheLine{"simpson13", integral, nint}]; ok { // está na cache ?
+				c.JSON(http.StatusOK, gin.H{"result": r})
+			} else { // não foi computado ainda
+				result, err := metodos.RegraDeSimpson13(integral, nint)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				} else {
+					cache[cacheLine{"simpson13", integral, nint}] = result
 					c.JSON(http.StatusOK, gin.H{"result": result})
 				}
 			}
--- metodos/metodos.go
+++ metodos/metodos.go
@@ -1,6 +1,8 @@
 package metodos
 
 import (
+	"fmt"
+
 	"github.com/fuzzyqu/trabalho-metodos/models"
 	"github.com/soudy/mathcat"
 )
@@ -39,6 +41,56 @@ func RegraDosTrapezios(integral models.Integral, n int64) (float64, error) {
 	}
 
 	result *= (deltaX / 2)
+
+	return result, nil
+}
+
+func RegraDeSimpson13(integral models.Integral, n int64) (float64, error) {
+
+	if n&1 != 0 {
+		return 0.0, fmt.Errorf("n must be even n: %d", n)
+	}
+
+	step := (integral.B - integral.A) / float64(n)
+
+	var result float64
+	params := make(map[string]float64, 8)
+
+	// a
+	params[integral.Parametro] = integral.A
+	r, err := mathcat.Exec(integral.Expressao, params)
+	if err != nil {
+		return r, err
+	}
+	result += r
+
+	// b
+	params[integral.Parametro] = integral.B
+	r, err = mathcat.Exec(integral.Expressao, params)
+	if err != nil {
+		return r, err
+	}
+	result += r
+
+	// intervalo
+	even := true
+	for i := integral.A + step; i < integral.B; i += step {
+		params[integral.Parametro] = i
+		r, err := mathcat.Exec(integral.Expressao, params)
+		if err != nil {
+			return r, err
+		}
+		if even {
+			result += r * 4
+			even = !even
+		} else {
+			result += r * 2
+			even = !even
+		}
+
+	}
+
+	result *= (step / 3)
 
 	return result, nil
 }
