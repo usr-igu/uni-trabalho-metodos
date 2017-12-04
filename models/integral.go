d76ec9a1fddf11594424eafbd39201fe107c7561
fuzzyqu <fuzzyqu@protonmail.com>
2017-12-03 21:07:17 -0300

    refatoração

--- models/integral.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Integral struct {
-	Expressao string  `json:"expressao"`
-	Parametro string  `json:"parametro"`
-	A         float64 `json:"a,string"`
-	B         float64 `json:"b,string"`
-}
